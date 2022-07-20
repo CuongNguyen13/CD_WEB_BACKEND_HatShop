d4237c23f265a9a283e1da04c995ae4801a4f72d
Nguyễn Văn Cương <67322685+CuongNguyen13@users.noreply.github.com>
2022-07-20 20:05:40 +0700

    xong usser

--- HatShop_BE/src/main/java/com/example/demo/controller/UserController.java
+++ HatShop_BE/src/main/java/com/example/demo/controller/UserController.java
@@ -6,10 +6,7 @@ import com.example.demo.entity.Products;
 import com.example.demo.entity.User;
 import com.example.demo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,9 +20,10 @@ public class UserController {
     public ListPage<UserDTO> getListContact(@RequestParam(name = "limit",required = false,defaultValue = "5") int limit, @RequestParam(name = "page",required = false,defaultValue = "1") int page){
         return userService.findAll(page,limit);
     }
-//@GetMapping("/admin/user")
-//public String getListContact(@RequestParam(name = "limit",required = false,defaultValue = "5") int limit, @RequestParam(name = "page",required = false,defaultValue = "1") int page){
-//    return "123";
-//}
+
+    @PostMapping("/admin/block")
+    public boolean blockUser(@RequestBody int id){
+        return userService.blockUser(id);
+    }
 
 }
