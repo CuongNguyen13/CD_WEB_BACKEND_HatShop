f3d701141079fbb26a2cf0e5cdf46bef71a22b43
haphamufc <65293225+haphamufc@users.noreply.github.com>
2022-07-31 19:48:26 +0700

    Commit validate

--- HatShop_BE/src/main/java/com/example/demo/controller/RegisterController.java
+++ HatShop_BE/src/main/java/com/example/demo/controller/RegisterController.java
@@ -1,4 +1,5 @@
 package com.example.demo.controller;
+
 import com.example.demo.model.UserModel;
 import com.example.demo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,20 +13,19 @@ import javax.validation.Valid;
 @RestController
 public class RegisterController {
     @Autowired
-    UserService userService ;
+    UserService userService;
 
     @RequestMapping(value = "/register")
-    public ModelAndView register(@Valid @ModelAttribute(name = "user") UserModel user, BindingResult result, ModelMap model){
-        if(result.hasErrors()){
+    public ModelAndView register(@Valid @ModelAttribute(name = "user") UserModel user, BindingResult result, ModelMap model) {
+        if (result.hasErrors()) {
 //            model.addAttribute("listDay", createDate.createDays());
 //            model.addAttribute("listMonth" , createDate.createMonths());
 //            model.addAttribute("listYear", createDate.createYears());
-            return new ModelAndView("register", model) ;
-        }
-        else {
-            if(userService.saveUser(user)){
-                model.addAttribute("register" ,true);
-                return new ModelAndView("redirect:/login" , model) ;
+            return new ModelAndView("register", model);
+        } else {
+            if (userService.saveUser(user)) {
+                model.addAttribute("register", true);
+                return new ModelAndView("redirect:/login", model);
             }
         }
         model.addAttribute("internal", true);
@@ -33,20 +33,31 @@ public class RegisterController {
     }
 
     @RequestMapping(value = "/register2")
-    public UserModel register2(@Valid @RequestBody UserModel user, BindingResult result){
-        if(result.hasErrors()){
+    public UserModel register2(@Valid @RequestBody UserModel user, BindingResult result) {
+        if (result.hasErrors()) {
             System.out.println("No1");
             return new UserModel();
-        }
-        else {
-            if(userService.saveUser(user)){
+        } else {
+            if (userService.saveUser(user)) {
                 System.out.println("Success = " + user);
-                return user ;
+                return user;
             }
         }
         System.out.println("No2");
         return new UserModel();
     }
+
+    @GetMapping(value = "/checkEmailExist")
+    public boolean register2(@RequestParam String email) {
+        System.out.println("email = " + email);
+        System.out.println("user = " + userService.findByEmail(email));
+        if (userService.findByEmail(email) != null) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
 }
 
 //{
